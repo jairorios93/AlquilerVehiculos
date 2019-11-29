d1cbc12f734635ed536392c4e5a4f19910b62e1f
Jairo Rios <jairo.rios@ceiba.com.co>
2019-11-29 12:22:29 -0500

    Se agrega clase prueba

---
+++ AlquilerVehiculos/src/test/java/com/ceiba/AlquilerVehiculos/AlquilerVehiculosTest.java
@@ -0,0 +1,14 @@
+package com.ceiba.AlquilerVehiculos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlquilerVehiculosTest {
+
+	@Test
+	void test() {
+		assertEquals(true, true);
+	}
+
+}
