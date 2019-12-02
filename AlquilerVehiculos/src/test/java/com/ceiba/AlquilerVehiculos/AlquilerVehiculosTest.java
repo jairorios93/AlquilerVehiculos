4ca8b1bfcd360d0aa7b6d3d5e946b57a9f63b5e8
jairo.rios <jairo.rios@ceiba.com.co>
2019-12-02 16:43:46 -0500

    Se modifican las pruebas

--- AlquilerVehiculos/src/test/java/com/ceiba/AlquilerVehiculos/AlquilerVehiculosTest.java
+++ AlquilerVehiculos/src/test/java/com/ceiba/AlquilerVehiculos/AlquilerVehiculosTest.java
@@ -2,15 +2,9 @@ package com.ceiba.AlquilerVehiculos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AlquilerVehiculosTest {
-
-	@BeforeEach
-	public void iniciar() {
-
-	}
 
 	@Test
 	public void test() {
