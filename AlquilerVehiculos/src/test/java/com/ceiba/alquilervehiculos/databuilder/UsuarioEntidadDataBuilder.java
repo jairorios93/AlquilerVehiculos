2c9a3be238b6a7992c4b8ee42f783b1bc137991d
jairo.rios <jairo.rios@ceiba.com.co>
2019-12-04 11:38:37 -0500

    Se agrega estructura para registro de usuario

---
+++ AlquilerVehiculos/src/test/java/com/ceiba/alquilervehiculos/databuilder/UsuarioEntidadDataBuilder.java
@@ -0,0 +1,44 @@
+package com.ceiba.alquilervehiculos.databuilder;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.ceiba.alquilervehiculos.infrastructura.entidades.UsuarioEntidad;
+
+public class UsuarioEntidadDataBuilder {
+
+	private String cedula;
+
+	private String nombres;
+
+	private String apellidos;
+
+	private Date fechaNacimiento;
+
+	public UsuarioEntidadDataBuilder() {
+		cedula = "1094935130";
+		nombres = "Jairo Andres";
+		apellidos = "Rios Franco";
+		fechaNacimiento = Calendar.getInstance().getTime();
+	}
+
+	public UsuarioEntidad conCedula(String cedula) {
+		return new UsuarioEntidad(cedula, nombres, apellidos, fechaNacimiento);
+	}
+
+	public UsuarioEntidad conNombres(String nombres) {
+		return new UsuarioEntidad(cedula, nombres, apellidos, fechaNacimiento);
+	}
+
+	public UsuarioEntidad conApellidos(String apellidos) {
+		return new UsuarioEntidad(cedula, nombres, apellidos, fechaNacimiento);
+	}
+
+	public UsuarioEntidad conFechaNacimiento(Date fechaNacimiento) {
+		return new UsuarioEntidad(cedula, nombres, apellidos, fechaNacimiento);
+	}
+
+	public UsuarioEntidad build() {
+		return new UsuarioEntidad(cedula, nombres, apellidos, fechaNacimiento);
+	}
+}
--- AlquilerVehiculos/src/test/java/com/ceiba/alquilervehiculos/infrastructura/respositorio/usuario/UsuarioRepositorioTest.java
+++ AlquilerVehiculos/src/test/java/com/ceiba/alquilervehiculos/infrastructura/respositorio/usuario/UsuarioRepositorioTest.java
@@ -1,5 +1,30 @@
 package com.ceiba.alquilervehiculos.infrastructura.respositorio.usuario;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import com.ceiba.alquilervehiculos.databuilder.UsuarioDataBuilder;
+import com.ceiba.alquilervehiculos.databuilder.UsuarioEntidadDataBuilder;
+import com.ceiba.alquilervehiculos.dominio.modelo.Usuario;
+import com.ceiba.alquilervehiculos.infrastructura.UsuarioRepositorioJPA;
+import com.ceiba.alquilervehiculos.infrastructura.adaptador.repositorio.RepositorioUsuarioPersistente;
+import com.ceiba.alquilervehiculos.infrastructura.entidades.UsuarioEntidad;
+
 public class UsuarioRepositorioTest {
 
+	@Test
+	void registrarUsuario() {
+		UsuarioEntidad usuarioEntidad = new UsuarioEntidadDataBuilder().build();
+		Usuario usuario = new UsuarioDataBuilder().build();
+
+		UsuarioRepositorioJPA usuarioRepositorioJPA = mock(UsuarioRepositorioJPA.class);
+
+		RepositorioUsuarioPersistente repositorioUsuarioPersistente = new RepositorioUsuarioPersistente(
+				usuarioRepositorioJPA);
+
+		assertDoesNotThrow(() -> usuarioRepositorioJPA.save(usuarioEntidad));
+		assertDoesNotThrow(() -> repositorioUsuarioPersistente.registrarUsuario(usuario));
+	}
 }
