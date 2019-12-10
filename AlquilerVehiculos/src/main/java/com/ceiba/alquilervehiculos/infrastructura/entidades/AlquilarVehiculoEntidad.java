49a080ca7a5f29b5b9091d208b7db60538088abd
jairo.rios <jairo.rios@ceiba.com.co>
2019-12-10 08:05:09 -0500

    Se agregan pruebas para devolver vechiculo

--- AlquilerVehiculos/src/main/java/com/ceiba/alquilervehiculos/infrastructura/entidades/AlquilarVehiculoEntidad.java
+++ AlquilerVehiculos/src/main/java/com/ceiba/alquilervehiculos/infrastructura/entidades/AlquilarVehiculoEntidad.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 public class AlquilarVehiculoEntidad {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
