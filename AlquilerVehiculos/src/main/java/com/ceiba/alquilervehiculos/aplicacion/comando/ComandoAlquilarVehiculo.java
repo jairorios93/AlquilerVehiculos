eab9330b378a5e3e9e1e146488c48012d5de6c51
jairo.rios <jairo.rios@ceiba.com.co>
2019-12-12 10:27:37 -0500

    Se eliminan getter del comando de alquiler

--- AlquilerVehiculos/src/main/java/com/ceiba/alquilervehiculos/aplicacion/comando/ComandoAlquilarVehiculo.java
+++ AlquilerVehiculos/src/main/java/com/ceiba/alquilervehiculos/aplicacion/comando/ComandoAlquilarVehiculo.java
@@ -36,56 +36,28 @@ public class ComandoAlquilarVehiculo {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public ComandoUsuario getUsuario() {
 		return usuario;
-	}
-
-	public void setUsuario(ComandoUsuario usuario) {
-		this.usuario = usuario;
 	}
 
 	public ComandoVehiculo getVehiculo() {
 		return vehiculo;
 	}
 
-	public void setVehiculo(ComandoVehiculo vehiculo) {
-		this.vehiculo = vehiculo;
-	}
-
 	public Date getFechaInicio() {
 		return fechaInicio;
-	}
-
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
 	}
 
 	public Date getFechaFin() {
 		return fechaFin;
 	}
 
-	public void setFechaFin(Date fechaFin) {
-		this.fechaFin = fechaFin;
-	}
-
 	public boolean isEstado() {
 		return estado;
 	}
 
-	public void setEstado(boolean estado) {
-		this.estado = estado;
-	}
-
 	public double getValor() {
 		return valor;
-	}
-
-	public void setValor(double valor) {
-		this.valor = valor;
 	}
 
 }
